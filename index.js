c802bcbd2def1d7c02529a16c2ec9ea4754b9a98
Carl Lei <xecycle@gmail.com>
2015-03-16 13:41:53 +0800

    Bugfix: inverted r/w.

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var openser = require("./build/Release/openser").openser;
 
 exports.openSync = function serial$openAsStream(path, baud) {
   var fd = openser(String(path), ~~baud);
-  return plexer(fs.createReadStream(null, { fd: fd }),
-                fs.createWriteStream(null, { fd: fd }));
+  return plexer(fs.createWriteStream(null, { fd: fd }),
+                fs.createReadStream(null, { fd: fd }));
 };
